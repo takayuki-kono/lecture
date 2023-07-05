b08bcf03147afbf3c3a450eb9ceede22388694c5
takayuki-kono <xx03dragon@gmail.com>
2023-07-05 20:11:09 +0900

    initial

---
+++ src/test/java/com/example/lecture/LectureApplicationTests.java
@@ -0,0 +1,15 @@
+package com.example.lecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LectureApplicationTests {
+
+	@Test
+	void contextLoads() {
+		int i = 0;
+		System.out.println(i + 1);
+	}
+
+}
