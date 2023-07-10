e90b3986ee0e8cb2071602772b6ed101cb26ddca
takayuki-kono <xx03dragon@gmail.com>
2023-07-10 17:13:57 +0900

    課題　20230710

---
+++ src/test/java/com/example/lecture/Test.java
@@ -0,0 +1,17 @@
+package com.example.lecture;
+
+import org.springframework.boot.SpringApplication;
+
+public class Test {
+
+    public static void main(String[] args) {
+        //20230710
+        //クラスのインスタンスを生成してください
+        //条件
+        //・Animal interfaceを実装したクラスDogを継承したPomeranianクラス
+        //・Dogクラスは毛の本数をパラメータにもつコンストラクタを定義する（Dogのhear変数に代入）
+        //・Pomeranianクラスは犬種をパラメータにもつコンストラクタを定義する（DogのkindOfDog変数に代入）
+        new BlackCat();
+    }
+
+}
