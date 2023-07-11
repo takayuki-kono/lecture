f2d8fa4f05ec1c762ee4414f989d514579593fda
ichi-hr3z-Sq <hr3z.o0w0sq.are@gmail.com>
2023-07-11 16:24:24 +0900

    寺田 進捗共有

--- src/test/java/com/example/lecture/Test.java
+++ src/test/java/com/example/lecture/Test.java
@@ -7,12 +7,29 @@ public class Test {
     public static void main(String[] args) {
         //20230710
         //クラスのインスタンスを生成してください
+        class Dog implements Animal {
+            int hear;
+            String kindOfDog;
+            Dog(int hear, String kindOfDog){
+                this.hear = hear;
+                this.kindOfDog = kindOfDog;
+            }
+
+            public void cry() {
+            }
+
+        }
+        class Pomeranian extends Dog{
+            Pomeranian(int hear){
+                super(hear);
+            }
+        }
         //条件
         //・Animal interfaceを実装したクラスDogを継承したPomeranianクラス
         //・Dogクラスは毛の本数と犬種をパラメータにもつコンストラクタを定義する（Dogのhear / kindOfDog変数に代入）
-        //・Pomeranianクラスは毛の本数をパラメータにもつコンストラクタを定義する
+        //・Pomeranianクラスは毛の本数をパラメータにもつコンストラクタを定義する（Dogのhear変数に代入）
         //・Animal interface にcryメソッドを定義する(非static)
-        new BlackCat();
+        //new BlackCat();
     }
 
 }
